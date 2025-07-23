9e097a32878c99e743066334712140994db95107
VictorNetto <vigonetto@gmail.com>
2025-07-22 21:55:03 -0300

    Replayed request/response now appears after the original request/response in the log. Added an option to choose to which domain the requests must be replayed

--- mitm-scripts/auth-test.py
+++ mitm-scripts/auth-test.py
@@ -6,6 +6,7 @@ possible diferences indicating the lack of proper authentication.
 
 from collections.abc import Sequence
 import logging
+import os
 
 from mitmproxy import ctx
 from mitmproxy import http
@@ -25,6 +26,10 @@ class Storage:
     def pop(self, id):
         return self.storage.pop(id)
 
+# Track request/response pairs (original and replayed one), comparing them as soon as possible
+# Request/reponse pairs are tracked using its flow.id in add_pair method
+# Comparison between them are made in add_content method, that store and compare responses's hash
+# After comparison, pairs data are cleared
 class Comparator:
     undefined = -1
     equal = 0
@@ -39,19 +44,20 @@ class Comparator:
         self.pairs[p2] = p1
     
     def add_content(self, p1, p1_content):
+        p1_content = hash(p1_content)
         self.content[p1] = p1_content
 
         p2 = self.pairs.get(p1)
         p2_content = self.content.get(p2)
 
-        result = (Comparator.undefined, p1, p2)
+        result = (Comparator.undefined, p2, p1)
         if p2_content is not None:
             if p1_content == p2_content:
                 logging.warning(f"Replayed request with equal responses: {p1} and {p2}")
-                result = (Comparator.equal, p1, p2)
+                result = (Comparator.equal, p2, p1)
             else:
                 logging.info(f"Replayed request with different responses: {p1} and {p2}")
-                result = (Comparator.different, p1, p2)
+                result = (Comparator.different, p2, p1)
             
             del self.pairs[p1]
             del self.pairs[p2]
@@ -64,6 +70,7 @@ class Duplicator:
     def __init__(self):
         self.storage = Storage()
         self.comparator = Comparator()
+        self.not_replayed = {}
 
     def load(self, loader: addonmanager.Loader):
         loader.add_option(
@@ -80,7 +87,19 @@ class Duplicator:
             help="Header to remove when replaying requests"
         )
 
+        loader.add_option(
+            name="replay_domain",
+            typespec=str,
+            default="",
+            help="Only replay requests to this domain"
+        )
+
     def request(self, flow: http.HTTPFlow):
+        replay_domain = ctx.options.replay_domain
+        if replay_domain != "" and replay_domain not in flow.request.host:
+            self.not_replayed[flow.id] = 'not replayed'
+            return
+
         raw_request = assemble.assemble_request(flow.request)
         self.storage.put_request(flow.id, raw_request)
 
@@ -109,11 +128,15 @@ class Duplicator:
         ctx.master.commands.call("replay.client", [flow])
     
     def response(self, flow: http.HTTPFlow):
+        if flow.id in self.not_replayed:
+            del self.not_replayed[flow.id]
+            return
+
         if flow.response and flow.response.content:
             raw_response = assemble.assemble_response(flow.response)
             self.storage.put_response(flow.id, raw_response)
 
-            result, flow_id1, flow_id2 = self.comparator.add_content(flow.id, hash(flow.response.content))
+            result, flow_id1, flow_id2 = self.comparator.add_content(flow.id, flow.response.content)
 
             dir_name = ''
             file_name = flow_id1 + '--' + flow_id2
@@ -131,5 +154,9 @@ class Duplicator:
                     request2 = self.storage.pop(flow_id2)
                     sep = b'\r\n' + b'-'*80 + b'\r\n\r\n'
                     file.write(request1 + sep + request2)
+
+# Create log directories if they not exist
+os.makedirs('flows/different', exist_ok=True)
+os.makedirs('flows/equal', exist_ok=True)
 
 addons = [Duplicator()]
