8d2145172db628a667b945c6bb2e1b22c70e01e2
VictorNetto <vigonetto@gmail.com>
2025-07-28 21:02:56 -0300

    Moved auth-test addon option to a yaml-file. Improved the response comparation, that now inspect status code e response body. Gziped response body are now unziped before being put on flows files

--- mitm-scripts/auth-test.py
+++ mitm-scripts/auth-test.py
@@ -13,95 +13,150 @@ from mitmproxy import http
 from mitmproxy import addonmanager
 from mitmproxy.net.http.http1 import assemble
 
-class Storage:
+class FlowStorage:
     def __init__(self):
-        self.storage = {}
+        self.raw_request: dict[str, bytes] = {}
+        self.raw_response: dict[str, bytes] = {}
+        self.hash_response_body: dict[str, int] = {}
+        self.status_code: dict[str, int] = {}
+
+    def put_raw_request(self, flow_id: str, raw_request: bytes):
+        self.raw_request[flow_id] = raw_request
+
+    def put_raw_response(self, flow_id: str, raw_response: bytes):
+        self.raw_response[flow_id] = raw_response
     
-    def put_request(self, id, data):
-        self.storage[id] = data
+    def put_response_body(self, flow_id: str, response_body: bytes):
+        self.hash_response_body[flow_id] = hash(response_body)
+    
+    def put_status_code(self, flow_id: str, status_code: int):
+        self.status_code[flow_id] = status_code
+    
+    def pop(self, flow_id: str):
+        data = self.raw_request[flow_id] + self.raw_response[flow_id]
 
-    def put_response(self, id, data):
-        self.storage[id] += data
+        del self.raw_request[flow_id]
+        del self.raw_response[flow_id]
+        del self.hash_response_body[flow_id]
+        del self.status_code[flow_id]
 
-    def pop(self, id):
-        return self.storage.pop(id)
+        return data
+    
+    def equal(self, flow_id1: str, flow_id2: str):
+        hash_response_body1 = self.hash_response_body.get(flow_id1, '')
+        hash_response_body2 = self.hash_response_body.get(flow_id2, '')
+        status_code1 = self.status_code.get(flow_id1, '')
+        status_code2 = self.status_code.get(flow_id2, '')
+
+        if hash_response_body1 == '' or hash_response_body2 == '' or \
+            status_code1 == '' or status_code2 == '':
+            return None
+
+        same_response_body = hash_response_body1 == hash_response_body2
+        same_status_code = status_code1 == status_code2
+
+        return same_response_body and same_status_code
 
 # Track request/response pairs (original and replayed one), comparing them as soon as possible
 # Request/reponse pairs are tracked using its flow.id in add_pair method
-# Comparison between them are made in add_content method, that store and compare responses's hash
-# After comparison, pairs data are cleared
+# Comparison between them are made in add_content method using the auxiliar FlowStorage instance
+#
 class Comparator:
     undefined = -1
     equal = 0
     different = 1
 
-    def __init__(self):
-        self.pairs = {}
-        self.content = {}
+    def __init__(self, flow_storage: FlowStorage):
+        self.pairs: dict[str, str] = {}
+        self.flow_storage = flow_storage
+        # self.content = {}
     
-    def add_pair(self, p1, p2):
-        self.pairs[p1] = p2
-        self.pairs[p2] = p1
+    def add_pair(self, flow_id1: str, flow_id2: str):
+        self.pairs[flow_id1] = flow_id2
+        self.pairs[flow_id2] = flow_id1
     
-    def add_content(self, p1, p1_content):
-        p1_content = hash(p1_content)
-        self.content[p1] = p1_content
+    def add_content(self, flow_id: str, response_body: bytes, status_code: int):
+        self.flow_storage.put_response_body(flow_id, response_body)
+        self.flow_storage.put_status_code(flow_id, status_code)
 
-        p2 = self.pairs.get(p1)
-        p2_content = self.content.get(p2)
+        other_flow_id = self.pairs.get(flow_id, '')
 
-        result = (Comparator.undefined, p2, p1)
-        if p2_content is not None:
-            if p1_content == p2_content:
-                logging.warning(f"Replayed request with equal responses: {p1} and {p2}")
-                result = (Comparator.equal, p2, p1)
+        result = (Comparator.undefined, '', '')
+
+        flows_are_equal = self.flow_storage.equal(flow_id, other_flow_id)
+        if flows_are_equal is not None:
+            if flows_are_equal:
+                logging.warning(f"Replayed request with equal responses: {other_flow_id} and {flow_id}")
+                result = (Comparator.equal, other_flow_id, flow_id)
             else:
-                logging.info(f"Replayed request with different responses: {p1} and {p2}")
-                result = (Comparator.different, p2, p1)
-            
-            del self.pairs[p1]
-            del self.pairs[p2]
-            del self.content[p1]
-            del self.content[p2]
+                logging.info(f"Replayed request with different responses: {other_flow_id} and {flow_id}")
+                result = (Comparator.different, other_flow_id, flow_id)
         
         return result
 
 class Duplicator:
     def __init__(self):
-        self.storage = Storage()
-        self.comparator = Comparator()
-        self.not_replayed = {}
+        self.flow_storage = FlowStorage()
+        self.comparator = Comparator(self.flow_storage)
+        self.not_replayed: dict[str, None] = {}
 
-    def load(self, loader: addonmanager.Loader):
-        loader.add_option(
-            name="cookie_name",
-            typespec=Sequence[str],
-            default=[],
-            help="Cookie to remove when replaying requests"
-        )
+        # Instead of setting options through command line, load them using the auth-test.yaml file
+        self.cookies = []
+        self.headers = []
+        self.domains = []
+        self.load_options()
+    
+    def load_options(self):
+        try:
+            with open('auth-test.yaml', 'r') as file:
+                logging.info("[*] Reading auth-test.yaml file")
+                lines = file.readlines()
 
-        loader.add_option(
-            name="header_name",
-            typespec=Sequence[str],
-            default=[],
-            help="Header to remove when replaying requests"
-        )
+                data = {
+                    'Cookies': [],
+                    'Headers': [],
+                    'Domains': [],
+                }
+                
+                state_machine = ''
+                for line in lines:
+                    if line.startswith('# Cookies'):
+                        state_machine = 'Cookies'
+                        continue
+                    elif line.startswith('# Headers'):
+                        state_machine = 'Headers'
+                        continue
+                    elif line.startswith('# Domains'):
+                        state_machine = 'Domains'
+                        continue
+                    elif line.startswith('#') or line.strip() == '':
+                        continue
 
-        loader.add_option(
-            name="replay_domain",
-            typespec=str,
-            default="",
-            help="Only replay requests to this domain"
-        )
+                    data[state_machine].append(line.strip())
+                
+                self.cookies = data['Cookies']
+                self.headers = data['Headers']
+                self.domains = data['Domains']
+
+                logging.info(f"[*] Loaded Cookies: {self.cookies}")
+                logging.info(f"[*] Loaded Headers: {self.headers}")
+                logging.info(f"[*] Loaded Domains: {self.domains}")
+
+        except FileNotFoundError:
+            logging.error("[!] auth-test.yaml file not found")
 
     def request(self, flow: http.HTTPFlow):
-        replay_domain = ctx.options.replay_domain
-        if replay_domain != "" and replay_domain not in flow.request.host:
-            self.not_replayed[flow.id] = 'not replayed'
+        replay_request = False
+        for domain in self.domains:
+            if domain in flow.request.host:
+                replay_request = True
+                break
+        if not replay_request:
+            self.not_replayed[flow.id] = None
             return
 
         raw_request = assemble.assemble_request(flow.request)
-        self.storage.put_request(flow.id, raw_request)
+        self.flow_storage.put_raw_request(flow.id, raw_request)
 
         # Avoid an infinite loop by not replaying already replayed requests
         if flow.is_replay == "request":
@@ -118,11 +173,11 @@ class Duplicator:
             ctx.master.commands.call("view.flows.duplicate", [flow])
         
         # Remove cookies from the duplicated request
-        for cookie in ctx.options.cookie_name:
+        for cookie in self.cookies:
             flow.request.cookies.pop(cookie, None)
 
         # Remove headers from the duplicated request
-        for header in ctx.options.header_name:
+        for header in self.headers:
             flow.request.headers.pop(header, None)
 
         ctx.master.commands.call("replay.client", [flow])
@@ -133,10 +188,11 @@ class Duplicator:
             return
 
         if flow.response and flow.response.content:
-            raw_response = assemble.assemble_response(flow.response)
-            self.storage.put_response(flow.id, raw_response)
+            raw_response_headers = assemble.assemble_response_head(flow.response)
+            raw_response = raw_response_headers + flow.response.content
+            self.flow_storage.put_raw_response(flow.id, raw_response)
 
-            result, flow_id1, flow_id2 = self.comparator.add_content(flow.id, flow.response.content)
+            result, flow_id1, flow_id2 = self.comparator.add_content(flow.id, flow.response.content, flow.response.status_code)
 
             dir_name = ''
             file_name = flow_id1 + '--' + flow_id2
@@ -150,8 +206,8 @@ class Duplicator:
                 dir_name = 'flows/different/'
 
             with open(dir_name + file_name + ext, 'wb') as file:
-                    request1 = self.storage.pop(flow_id1)
-                    request2 = self.storage.pop(flow_id2)
+                    request1 = self.flow_storage.pop(flow_id1)
+                    request2 = self.flow_storage.pop(flow_id2)
                     sep = b'\r\n' + b'-'*80 + b'\r\n\r\n'
                     file.write(request1 + sep + request2)
 
